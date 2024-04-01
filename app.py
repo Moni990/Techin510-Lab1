cb58c1de67f74c90bc44776cead0d976c8bacf20
Moni <monihuangpro@gmail.com>
2024-03-31 19:47:24 -0700

    init

--- app.py
+++ app.py
@@ -1,16 +1,12 @@
 import streamlit as st
 
-# Title of your profile page
 st.title("Hi, My name is Moni")
 
-# Introduction or About Me section
 st.header("About Me")
 st.write("I'm a passionate individual with design in HCI & UX")
 
-# Display an image
 st.image("./image/Moni.jpg")
 
-# Display contact information or any other personal details
 st.header("Contact Information")
 st.write("Email: monih@uw.edu")
 
